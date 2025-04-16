74bf1f45b6043dc83d595c40bf85190cc127afda
Miia <mkarhuma24@edu.oulu.fi>
2025-04-16 13:17:38 +0300

    Add files via upload

---
+++ Raspberry Pico code version 1 date 14.4/main.py
@@ -0,0 +1,38 @@
+from tm1637 import TM1637
+from machine import Pin, ADC
+from time import sleep
+import dht
+import math
+from ntc import read_ntc_temp
+
+display = TM1637(clk=Pin(2), dio=Pin(3))
+sensor = dht.DHT22(Pin(16))
+adc = ADC(Pin(26))
+
+while True:
+    try:
+        sensor.measure()
+        dht_temp = int(round(sensor.temperature()))
+        ntc_temp = int(round(read_ntc_temp()))
+
+        if ntc_temp < -9:
+            ntc_temp = -9
+        if ntc_temp > 99:
+            ntc_temp = 99
+
+        print("DHT22:", dht_temp, "°C | NTC:", ntc_temp, "°C")
+
+        dht_str = "{:02d}".format(dht_temp)
+        ntc_str = "{:02d}".format(ntc_temp)
+
+        display.show_colon(True)
+        display.show(dht_str + ntc_str)
+    except Exception as e:
+        print("ERROR:", e)
+        display.show("----")
+
+    sleep(1)
+
+
+
+
