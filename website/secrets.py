4157d2b007a5f2f98bbf34d99cc6b0f2603f14a6
Jorma17 <107813880+Jorma17@users.noreply.github.com>
2025-04-23 13:27:25 +0300

    Update `secrets.py`
    Now raises `ValueError` if SSID and password haven't been changed from default values

--- website/secrets.py
+++ website/secrets.py
@@ -1,4 +1,10 @@
+DEFAULT_SSID: str = "addYourself"
+DEFAULT_PASSWORD: str = "addYourself"
+
 secrets = {
-    'ssid': 'addYourself',
-    'pw': 'addYourself'
+    'ssid': DEFAULT_SSID,
+    'pw': DEFAULT_PASSWORD
 }
+
+if secrets["ssid"] == DEFAULT_SSID or secrets["pw"] == DEFAULT_PASSWORD:
+    raise ValueError("Change secrets.py SSID and password values to match your network credentials")
